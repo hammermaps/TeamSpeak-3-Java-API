9f4d311809a97ee36cca17611933298398730204
Your Name <godkiller_nt@hammermaps.de>
2026-03-25 22:34:18 +0100

    Make ChannelEditedEvent class final to prevent subclassing

--- src/main/java/com/github/theholywaffle/teamspeak3/api/event/ChannelEditedEvent.java
+++ src/main/java/com/github/theholywaffle/teamspeak3/api/event/ChannelEditedEvent.java
@@ -29,7 +29,7 @@ package com.github.theholywaffle.teamspeak3.api.event;
 import com.github.theholywaffle.teamspeak3.api.ChannelProperty;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Wrapper;
 
-public class ChannelEditedEvent extends BaseEvent {
+public final class ChannelEditedEvent extends BaseEvent {
 
 	public ChannelEditedEvent(Wrapper wrapper) {
 		super(wrapper);
