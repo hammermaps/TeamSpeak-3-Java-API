21d4a62674326d7d4bf75a5846c65644ea52d470
Your Name <godkiller_nt@hammermaps.de>
2026-03-25 22:35:00 +0100

    Make PrivilegeKeyUsedEvent class final to prevent subclassing

--- src/main/java/com/github/theholywaffle/teamspeak3/api/event/PrivilegeKeyUsedEvent.java
+++ src/main/java/com/github/theholywaffle/teamspeak3/api/event/PrivilegeKeyUsedEvent.java
@@ -29,7 +29,7 @@ package com.github.theholywaffle.teamspeak3.api.event;
 import com.github.theholywaffle.teamspeak3.api.PrivilegeKeyType;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Wrapper;
 
-public class PrivilegeKeyUsedEvent extends BaseEvent {
+public final class PrivilegeKeyUsedEvent extends BaseEvent {
 
 	public PrivilegeKeyUsedEvent(Wrapper wrapper) {
 		super(wrapper);
