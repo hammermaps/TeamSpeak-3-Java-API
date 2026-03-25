ec4d7e97daeb74e14f780822b8d2bc3b774a6a52
Your Name <godkiller_nt@hammermaps.de>
2026-03-25 22:35:58 +0100

    Add @Serial annotation to TS3Exception for serialization compatibility

--- src/main/java/com/github/theholywaffle/teamspeak3/api/exception/TS3Exception.java
+++ src/main/java/com/github/theholywaffle/teamspeak3/api/exception/TS3Exception.java
@@ -1,5 +1,7 @@
 package com.github.theholywaffle.teamspeak3.api.exception;
 
+import java.io.Serial;
+
 /*
  * #%L
  * TeamSpeak 3 Java API
@@ -28,6 +30,7 @@ package com.github.theholywaffle.teamspeak3.api.exception;
 
 public class TS3Exception extends RuntimeException {
 
+	@Serial
 	private static final long serialVersionUID = 7167169981592989359L;
 
 	public TS3Exception(String msg) {
